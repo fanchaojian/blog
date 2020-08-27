c48139edc4a7db48de9d198914949e3b0101a88d
unknown <1948556024@qq.com>
2020-08-27 23:06:12 +0800

    first push

---
+++ src/components/blog/top.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: 'top',
+  data () {
+    return {
+        msg:"这是我的top组件"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
